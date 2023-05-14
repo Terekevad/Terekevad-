41f044ac49d92897ca2aae5757264f9a103ed9f0
Terekevad <130385252+Terekevad@users.noreply.github.com>
2023-05-14 22:14:12 +0300

    loeng10

---
+++ loeng09/script.js
@@ -0,0 +1,11 @@
+let number = 0;
+let app = document.querySelector('#app');
+
+while (number < 10) {
+    console.log('Number: ' + number);
+    number++;
+    
+}
+for (let i = 0; i < 10; i++) {
+    console.log('for i: ' i);
+}
